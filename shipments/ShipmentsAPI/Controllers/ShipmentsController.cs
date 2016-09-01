a8965c7a62d816beebc20522eebe0971a2b454c9
HarryPaek <paek.harry@gmail.com>
2016-09-01 23:50:02 +0900

    Added WebAPI first code as Backend API

---
+++ shipments/ShipmentsAPI/Controllers/ShipmentsController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace ShipmentsAPI.Controllers
+{
+    [Authorize]
+    public class ShipmentsController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ shipments/ShipmentsAPI/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using ShipmentsAPI;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace ShipmentsAPI
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            WebApiConfig.Register(config);
+
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+    }
+}
